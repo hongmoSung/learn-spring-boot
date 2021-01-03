6fe189f6d4a9b28833048c6b9045566c348c5ef1
hongmo sung <tesung30@naver.com>
2021-01-03 20:26:19 +0900

    loggin

--- src/main/java/io/hongmo/SampleRunner.java
+++ src/main/java/io/hongmo/SampleRunner.java
@@ -1,5 +1,7 @@
 package io.hongmo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleRunner implements ApplicationRunner {
 
+    private Logger logger = LoggerFactory.getLogger(SampleRunner.class);
+
     @Autowired
     private String hello;
 
@@ -17,9 +21,9 @@ public class SampleRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("====================================");
-        System.out.println(hello);
-        System.out.println(hongmoProperties.getName());
-        System.out.println("====================================");
+        logger.debug("====================================");
+        logger.debug(hello);
+        logger.debug(hongmoProperties.getName());
+        logger.debug("====================================");
     }
 }
