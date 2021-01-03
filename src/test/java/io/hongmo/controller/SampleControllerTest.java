c99e9e69382514573b22debc48532c3ff4a752f3
hongmo sung <tesung30@naver.com>
2021-01-03 21:06:41 +0900

    webtestclient

--- src/test/java/io/hongmo/controller/SampleControllerTest.java
+++ src/test/java/io/hongmo/controller/SampleControllerTest.java
@@ -1,34 +1,37 @@
 package io.hongmo.controller;
 
+import io.hongmo.service.SampleService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class SampleControllerTest {
 
+    //    @Autowired
+//    TestRestTemplate testRestTemplate;
     @Autowired
-    MockMvc mockMvc;
+    WebTestClient webTestClient;
+
+    @MockBean
+    SampleService sampleService;
 
     @Test
     public void hello() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hellohongmo"))
-                .andDo(print());
+
+        when(sampleService.getName()).thenReturn(" hongmo");
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello hongmo");
     }
 
 }
